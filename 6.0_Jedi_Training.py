c40c9a46146c2710c137a1fab6d5e3dd45f914ea
neyensian <neyensian@urbandaleschools.com>
2020-11-19 10:55:24 -0600

    asdf

--- 6.0_Jedi_Training.py
+++ 6.0_Jedi_Training.py
@@ -1,21 +1,34 @@
 '''
-Sign your name:________________
+Sign your name:Ian Neyens
 
 
-1.) Convert Binary 101010 to Hexadecimal.
+1.) Convert Binary 101010 to Hexadecimal.       2A
 
-2.) Convert Binary 11101001 to Octal.
+2.) Convert Binary 11101001 to Octal.       351
 
-3.) Convert Hexadecimal FC to Binary.
+3.) Convert Hexadecimal FC to Binary.       11111100
 
-4.) Convert Hexadecimal 1F to Decimal.
+4.) Convert Hexadecimal 1F to Decimal.      31
 
-5.) Convert Hexadecimal #FAAFBD to RGB.
+5.) Convert Hexadecimal #FAAFBD to RGB.     (250,175,189)
 
-6.) Convert Octal 70 to Binary.
+6.) Convert Octal 70 to Binary.     111000
 
-7.) Convert RGB (32,128,64) to Hexadecimal.
+7.) Convert RGB (32,128,64) to Hexadecimal.     208040
 
 8.) Convert 01000001, 01010011, 01000011, 01001001, 01001001 to ASCII.
+            65 - A     83 - S    67 - C    73 - I    73 - I
+
+
+01010100    84      T
+01101000    104     H
+01100001    97      Ae
+01101110    110     N
+01101011    107     K
+00100000    32   [space]
+01111001    121     Y
+01101111    111     O
+01110101    117     U
+00100001    33      !
 
 '''
